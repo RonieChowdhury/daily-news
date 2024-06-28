f4bf1a0856c6105588b75ac56994e884076de2dd
Raka Chowdhury <raka.chowdhury@autodesk.com>
2024-06-28 14:40:24 +0530

    Networking with Ktor as shared business logic

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -2,8 +2,9 @@ plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidLibrary)
     id("co.touchlab.skie") version "0.6.4"
+    kotlin("plugin.serialization") version "1.9.20"
 }
-
+@OptIn(org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi::class)
 kotlin {
     androidTarget {
         compilations.all {
@@ -26,13 +27,18 @@ kotlin {
 
     sourceSets {
         commonMain.dependencies {
-            implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+            implementation(libs.kotlinx.coroutines.core)
+            implementation(libs.ktor.client.core)
+            implementation(libs.ktor.client.content.negotiation)
+            implementation(libs.ktor.serialization.kotlinx.json)
+            implementation(libs.kotlinx.datetime)
         }
         androidMain.dependencies {
-            implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
+            implementation(libs.androidx.lifecycle.viewmodel.ktx)
+            implementation(libs.ktor.client.android)
         }
         iosMain.dependencies {
-
+            implementation(libs.ktor.client.darwin)
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
